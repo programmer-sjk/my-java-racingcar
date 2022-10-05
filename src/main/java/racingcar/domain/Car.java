18afa41bd4ba2cec3763921d83408203e61a5350
sjk5766 <wjdrnr6662@gmail.com>
2022-10-05 16:58:45 +0900

    feat(domain): 자동차 이름이 5자를 초과할 경우 예외발생

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,12 +1,20 @@
 package racingcar.domain;
 
 public class Car {
+    private static final int NAME_LIMIT = 5;
     private String name;
     public Car(String name) {
+        validate(name);
         this.name = name;
     }
 
     public String getName() {
         return this.name;
+    }
+
+    private void validate(String name) {
+        if (name.length() > NAME_LIMIT) {
+            throw new IllegalArgumentException("자동차의 이름은 5자를 초과할 수 없습니다");
+        }
     }
 }
--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -2,8 +2,11 @@ package racingcar.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class CarTest {
     @DisplayName("자동차 객체에 전달된 입력값이 이름이 된다")
@@ -11,5 +14,15 @@ public class CarTest {
     void 자동차_이름_테스트() {
         Car car = new Car("자동차이름");
         assertThat(car.getName()).isEqualTo("자동차이름");
+    }
+
+    @DisplayName("자동차 이름이 5자를 초과하면 예외가 발생한다")
+    @ParameterizedTest
+    @ValueSource(strings = {"6개의이름차", "7개의이름자동", "8개의이름자동차"})
+    void 자동차_이름길이_테스트(String input) {
+        assertThatThrownBy(() -> {
+            new Car(input);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차의 이름은 5자를 초과할 수 없습니다");
     }
 }
